d2097d539d45a9453d95c85ddd2807b45cba173d
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:50:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,13 +55,9 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT * FROM students WHERE name = ? LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
   end
 
   def update
