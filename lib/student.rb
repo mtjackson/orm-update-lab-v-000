73087668df075abb03c8b03923f030f725132e53
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:44:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,4 +28,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+
 end
